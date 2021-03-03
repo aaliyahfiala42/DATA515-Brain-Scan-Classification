2e9fa778db7ab56af759325c5d8b63ec230384eb
sabrinawang06 <lxw5332@uw.edu>
2021-03-02 17:36:03 -0800

    Link the web app to the new model and move the html/static folder under the brain scan

--- brain_scan/prediction.py
+++ brain_scan/prediction.py
@@ -1,19 +1,21 @@
 import tensorflow as tf
-from keras.preprocessing import image
+import cv2
 import numpy as np
 
 
-# Take the uploaded images and perform prediction
-# (model is loaded as a .h5 file)
+# Take the uploaded images and perform prediction (model is loaded as a .h5 file)
 def prediction(path):
-    model = tf.keras.models.load_model('my_model.h5')
-    test_img = image.load_img(
-                "static\\uploads\\" + path, target_size=(150, 150))
-    test_img = image.img_to_array(test_img)
-    test_img = np.expand_dims(test_img, axis=0)
-    images = np.vstack([test_img])
-    classes = model.predict(images, batch_size=10)
-    if classes[0][0] == 1:
+    model = tf.keras.models.load_model('./final_model.h5')
+    image = cv2.imread("./static/uploads/"+path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Reshape the image so that it fits the model required input format
+    image = cv2.resize(image, dsize=(240, 240), interpolation=cv2.INTER_CUBIC)
+    test_array = np.array(image)
+    reshape_array = test_array.reshape(-1, 240, 240, 1)
+    predictions = model.predict(reshape_array, batch_size=32)
+
+    if predictions[0][0] == 1:
         return ('Yes')
     else:
         return ("No")
