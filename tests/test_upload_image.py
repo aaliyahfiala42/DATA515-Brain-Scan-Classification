7dee156e5f3a002890cc8d225634bf399b028b1e
aly-such <77369888+aly-such@users.noreply.github.com>
2021-03-08 17:09:47 -0500

    Unit Test for Upload Image (May Have Errors)

---
+++ tests/test_upload_image.py
@@ -0,0 +1,40 @@
+import unittest
+import sys
+# sys.path.append('../brain_scan/')
+# import main
+from brain_scan import application
+
+class TestUploadImage(unittest.TestCase):
+    def setup(self):
+        self.app = main.app.test_client()
+        
+    def test_upload_image_get(self):
+        rv = self.app.get('/')
+        self.assertTrue('<!-- Home Page/Upload picture for prediction -->' in str(rv.data))
+        
+    def test_upload_image_post(self):
+        rv = self.app.post('/')
+        self.assertTrue('302 FOUND' == str(rv.data))
+        
+    def test_upload_image_file_not_in_request(self):
+        results = application.upload_image(file)
+        
+        self.assertEquals(results, redirect(request.url))
+    
+    def test_upload_image_empty_filename(self):
+        file = ''
+        results = application.upload_image(file)
+        
+        self.assertEquals(results, redirect(request.url))
+        
+    def test_allowed_image_upload(self):
+        file = 'img.jpeg'
+        results = application.upload_image(file)
+        
+        self.assertEquals(results, render_template('upload.html', filename=filename))
+        
+    `def test_not_allowed_image_upload(self):
+        file = 'img'
+        results = application.upload_image(file)
+        
+        self.assertEquals(results, redirect(request.url))
