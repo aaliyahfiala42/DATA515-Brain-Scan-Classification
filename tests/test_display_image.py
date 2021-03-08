e8a9a8a6d8c13bc290ba87996afc4b63adab512d
vaneshsu99 <vaneshsu@uw.edu>
2021-03-07 18:00:03 -0800

    updating import

--- tests/test_display_image.py
+++ tests/test_display_image.py
@@ -2,7 +2,7 @@ import unittest
 import sys
 # sys.path.append('../brain_scan/')
 # import main
-from brain_scan import application
+from brain_scan import app
 
 class TestDisplayImage(unittest.TestCase):
     def setUp(self):
--- tests/test_team_page.py
+++ tests/test_team_page.py
@@ -2,7 +2,7 @@ import unittest
 import sys
 # sys.path.append('../brain_scan/')
 # import main
-from brain_scan import application
+from brain_scan import app
 
 class TestAboutPage(unittest.TestCase):
     def setUp(self):
