30c93b341e79d65cf321d187161328a2e0a0944f
aly-such <77369888+aly-such@users.noreply.github.com>
2021-03-08 17:08:14 -0500

    changed main.allowed_file(...) to application.allowed_file(...)

--- tests/test_allowed_file.py
+++ tests/test_allowed_file.py
@@ -7,12 +7,12 @@ from brain_scan import application
 class TestAllowedFile(unittest.TestCase):
     def test_allowed_file_true(self):
        good_filenames = ['img.jpg','img.png','img.jpeg','img.gif']
-       results = [main.allowed_file(filename) for filename in good_filenames]
+       results = [application.allowed_file(filename) for filename in good_filenames]
 
        self.assertEqual(results, [True, True, True, True])
 
     def test_allowed_file_false(self):
         bad_filenames = ['imgjpg', 'img.jp', 'imgpng']
-        results = [main.allowed_file(filename) for filename in bad_filenames]
+        results = [application.allowed_file(filename) for filename in bad_filenames]
 
         self.assertEqual(results, [False, False, False])
