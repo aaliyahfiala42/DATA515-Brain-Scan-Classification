d2a67e92a4d4e450b94a087f89f6dbaa7d27e875
vaneshsu99 <vaneshsu@uw.edu>
2021-03-07 18:20:26 -0800

    attempting to set path for relative imports

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,2 @@
-import sys, os
-path = os.path.dirname(__file__)
-path = os.path.join(path, 'brain-scan')
-if path not in sys.path:
-    sys.path.append(path)
+import sys
+sys.path.append('../brain-scan/')
