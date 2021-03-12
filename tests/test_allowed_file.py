33eb154c538ff1ebb34132f59c9c8ac0f63fa7fb
Jordan <jjfields@uw.edu>
2021-03-12 15:42:58 -0800

    fixed typo in test_upload_image.py

--- tests/test_allowed_file.py
+++ tests/test_allowed_file.py
@@ -4,10 +4,10 @@ from brain_scan import app
 
 class TestAllowedFile(unittest.TestCase):
     def test_allowed_file_true(self):
-        good_filenames = ['img.jpg', 'img.png', 'img.jpeg', 'img.gif']
+        good_filenames = ['img.jpg', 'img.png', 'img.jpeg']
         results = [app.allowed_file(filename) for filename in good_filenames]
 
-        self.assertEqual(results, [True, True, True, True])
+        self.assertEqual(results, [True, True, True])
 
     def test_allowed_file_false(self):
         bad_filenames = ['imgjpg', 'img.jp', 'imgpng']
--- tests/test_upload_image.py
+++ tests/test_upload_image.py
@@ -10,6 +10,6 @@ class TestUploadImage(unittest.TestCase):
         self.client = self.app.test_client()
         
     def test_upload_image(self):
-        res = self.client.post('/', data = dict(file = (io.BytesIO(n"This is a test"), 'test.jpeg'),), follow_redirects = True)
+        res = self.client.post('/', data = dict(file = (io.BytesIO(b"This is a test"), 'test.jpeg'),), follow_redirects = True)
         assert res.status_code == 200
         assert 'file saved' in res.data
