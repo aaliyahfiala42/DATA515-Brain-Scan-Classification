78a6dcf8c845f5c2535cb9cd47f6195d8b564208
Jordan <jjfields@uw.edu>
2021-03-11 18:44:36 -0800

    fixed flake8 violations in setup.py

--- setup.py
+++ setup.py
@@ -4,26 +4,26 @@ with open('README.md', 'r') as f:
     long_description = f.read()
 
 REQUIREMENTS = ['Flask',
-		'Keras',
-		'numpy',
-		'tensorflow',
-		'ujson',
-		'unicodecsv',
-		'urllib3',
-		'webencodings',
-		'Werkzeug']
+                'Keras',
+                'numpy',
+                'tensorflow',
+                'ujson',
+                'unicodecsv',
+                'urllib3',
+                'webencodings',
+                'Werkzeug']
 
 
 setuptools.setup(
-   name = 'brain_scan',
-   version = '0.1.0',
-   description = 'A brain scan tumor classification model.',
-   license = 'MIT',
-   long_description = long_description,
-   author = 'DATA 515 Brain Scan Classification Team',
-   author_email = 'fialaa@uw.edu',
-   url = "http://aaliyahhanni.pythonanywhere.com/",
+   name='brain_scan',
+   version='0.1.0',
+   description='A brain scan tumor classification model.',
+   license='MIT',
+   long_description=long_description,
+   author='DATA 515 Brain Scan Classification Team',
+   author_email='fialaa@uw.edu',
+   url="http://aaliyahhanni.pythonanywhere.com/",
    python_requires='>=3.6',
-   packages = setuptools.find_packages('brain_scan'),
-   install_requires = REQUIREMENTS
+   packages=setuptools.find_packages('brain_scan'),
+   install_requires=REQUIREMENTS
 )
