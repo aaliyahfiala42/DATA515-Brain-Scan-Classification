6d1904d044f016c7a6ea9e15b83f8aeeee83bfb3
Aaliyah Fiala <aaliyahfiala42@gmail.com>
2021-03-12 19:54:42 -0800

    Updates to setup.py
    numpy == 1.19.2
    add openvc-python
    remove "brain_scan" from the find_package
    python version <3.9
    updated team emails

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ with open('README.md', 'r') as f:
 
 REQUIREMENTS = ['Flask',
                 'Keras',
-                'numpy',
+                'numpy==1.19.2',
                 'tensorflow',
                 'ujson',
                 'unicodecsv',
                 'urllib3',
                 'webencodings',
-                'Werkzeug']
+                'Werkzeug',
+                'opencv-python']
 
 
 setuptools.setup(
@@ -21,9 +22,10 @@ setuptools.setup(
    license='MIT',
    long_description=long_description,
    author='DATA 515 Brain Scan Classification Team',
-   author_email='fialaa@uw.edu',
+   author_email='fialaa@uw.edu, ams884@uw.edu, jjfields@uw.edu>, \
+        lxw5332@uw.edu, nimstre@uw.edu, vaneshsu@uw.edu',
    url="http://doihaveatumor.com/",
-   python_requires='>=3.6, <=3.8',
+   python_requires='>=3.6, <3.9',
    packages=setuptools.find_packages(),
    install_requires=REQUIREMENTS
 )
