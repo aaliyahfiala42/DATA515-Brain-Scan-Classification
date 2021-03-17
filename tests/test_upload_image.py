a5748863cb109a309aed2c86673a365e8a9d2dfc
Jordan <jjfields@uw.edu>
2021-03-16 21:00:12 -0700

    fixed flake8 violations in a unit test

--- tests/test_upload_image.py
+++ tests/test_upload_image.py
@@ -1,5 +1,4 @@
 import unittest
-from io import BytesIO
 from brain_scan import app
 
 
@@ -10,16 +9,10 @@ class TestUploadImage(unittest.TestCase):
         self.client = self.app.test_client()
 
     def test_upload_image_success(self):
-        '''
-        This test is supposed to pass a file and verify that a prediction
-        was given. Seems file name is not correctly passed to the client
-        So we are unable to hit the if statement in upload_image
-        that checks for the existence of file and that it has a 
-        valid name.
-        '''
-
         with open('tests/mock_data/no.jpg', 'rb') as data:
-            response = self.client.post('/', content_type='multipart/form-data',
-                                        data={'file': data}, follow_redirects=True)
+            response = self.client.post('/',
+                                        content_type='multipart/form-data',
+                                        data={'file': data},
+                                        follow_redirects=True)
             self.assertTrue('The prediction is'
                             in str(response.data))
