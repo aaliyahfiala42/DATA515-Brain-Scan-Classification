c54eca5521e3231e99f6fc6df847c8757f29b587
aly-such <77369888+aly-such@users.noreply.github.com>
2021-03-17 20:43:02 -0400

    Update test_model_pre_process_file.py

--- tests/test_model_pre_process_file.py
+++ tests/test_model_pre_process_file.py
@@ -12,13 +12,3 @@ class TestModelPreProcessFile(unittest.TestCase):
 
         self.assertTrue(len(im) == 240)
         self.assertTrue(label == 0)
-
-    def test_pre_process_file_yes(self):
-        my_model = model.Model()
-        directory = 'tests/mock_data'
-        filename = 'yes.jpg'
-
-        im, label = my_model._Model__pre_process_file(directory, filename)
-
-        self.assertTrue(len(im) == 240)
-        self.assertTrue(label == 1)
